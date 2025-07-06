9eb40c11d8f7d9326cc9d0efebc399c5499793b1
Chuji <64228911+Choojermelon@users.noreply.github.com>
2025-07-06 21:39:40 +0100

    V0.1 base functionality (Import, Export, Delete, Reorder)

--- DawnsburyCharacterManager/Form1.Designer.cs
+++ DawnsburyCharacterManager/Form1.Designer.cs
@@ -28,13 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lstCharacters = new System.Windows.Forms.ListBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnOpenLibrary = new System.Windows.Forms.Button();
+            this.btnMoveUp = new System.Windows.Forms.Button();
+            this.btnMoveDown = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstCharacters
+            // 
+            this.lstCharacters.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstCharacters.FormattingEnabled = true;
+            this.lstCharacters.ItemHeight = 16;
+            this.lstCharacters.Location = new System.Drawing.Point(12, 13);
+            this.lstCharacters.Name = "lstCharacters";
+            this.lstCharacters.Size = new System.Drawing.Size(274, 420);
+            this.lstCharacters.TabIndex = 0;
+            this.lstCharacters.SelectedIndexChanged += new System.EventHandler(this.lstCharacters_SelectedIndexChanged);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.Location = new System.Drawing.Point(384, 397);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(86, 36);
+            this.btnExport.TabIndex = 1;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.buttonExport_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImport.Location = new System.Drawing.Point(292, 397);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(86, 36);
+            this.btnImport.TabIndex = 2;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(488, 21);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(135, 20);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "(Status Message)";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(476, 397);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(86, 36);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnOpenLibrary
+            // 
+            this.btnOpenLibrary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenLibrary.Location = new System.Drawing.Point(292, 13);
+            this.btnOpenLibrary.Name = "btnOpenLibrary";
+            this.btnOpenLibrary.Size = new System.Drawing.Size(190, 36);
+            this.btnOpenLibrary.TabIndex = 5;
+            this.btnOpenLibrary.Text = "Open Character Library";
+            this.btnOpenLibrary.UseVisualStyleBackColor = true;
+            this.btnOpenLibrary.Click += new System.EventHandler(this.btnOpenLibrary_Click);
+            // 
+            // btnMoveUp
+            // 
+            this.btnMoveUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoveUp.Location = new System.Drawing.Point(646, 355);
+            this.btnMoveUp.Name = "btnMoveUp";
+            this.btnMoveUp.Size = new System.Drawing.Size(107, 36);
+            this.btnMoveUp.TabIndex = 6;
+            this.btnMoveUp.Text = "Move Up";
+            this.btnMoveUp.UseVisualStyleBackColor = true;
+            this.btnMoveUp.Click += new System.EventHandler(this.btnMoveUp_Click);
+            // 
+            // btnMoveDown
+            // 
+            this.btnMoveDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMoveDown.Location = new System.Drawing.Point(646, 397);
+            this.btnMoveDown.Name = "btnMoveDown";
+            this.btnMoveDown.Size = new System.Drawing.Size(107, 36);
+            this.btnMoveDown.TabIndex = 7;
+            this.btnMoveDown.Text = "Move Down";
+            this.btnMoveDown.UseVisualStyleBackColor = true;
+            this.btnMoveDown.Click += new System.EventHandler(this.btnMoveDown_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(765, 450);
+            this.Controls.Add(this.btnMoveDown);
+            this.Controls.Add(this.btnMoveUp);
+            this.Controls.Add(this.btnOpenLibrary);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.lstCharacters);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Dawnsbury Character Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstCharacters;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnOpenLibrary;
+        private System.Windows.Forms.Button btnMoveUp;
+        private System.Windows.Forms.Button btnMoveDown;
     }
 }
 
--- DawnsburyCharacterManager/Form1.cs
+++ DawnsburyCharacterManager/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,247 @@ namespace DawnsburyCharacterManager
 {
     public partial class Form1 : Form
     {
+        private string libraryPath = "characterLibrary.json";
+        private JObject characterLibrary;
+        private JArray profiles;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnExport.Enabled = false;
+            btnDelete.Enabled = false;
+            btnImport.Enabled = false;
+            btnMoveDown.Enabled = false;
+            btnMoveUp.Enabled = false;
+
+            lblStatus.Text = "Open a Character Library to begin!";
+        }
+
+        private void buttonExport_Click(object sender, EventArgs e)
+        {
+            if (lstCharacters.SelectedIndex == -1)
+            {
+                MessageBox.Show("Select a character first.");
+                return;
+            }
+
+            var selectedName = lstCharacters.SelectedItem.ToString();
+
+            var profile = profiles.FirstOrDefault(p => p["SelectedFeats"]?["Root:Identity"]?["Name"]?.ToString() == selectedName);
+
+            if (profile == null)
+            {
+                MessageBox.Show("Character not found.");
+                return;
+            }
+
+            SaveFileDialog saveDialog = new SaveFileDialog
+            {
+                Filter = "JSON Files (*.json)|*.json",
+                FileName = $"{selectedName}.json"
+            };
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveDialog.FileName, JsonConvert.SerializeObject(profile, Formatting.Indented));
+                lblStatus.Text = $"Exported {selectedName}.";
+            }
+        }
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog
+            {
+                Filter = "JSON Files (*.json)|*.json"
+            };
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                var json = File.ReadAllText(openDialog.FileName);
+                var newProfile = JObject.Parse(json);
+
+                string newName = newProfile["SelectedFeats"]?["Root:Identity"]?["Name"]?.ToString();
+                if (string.IsNullOrWhiteSpace(newName))
+                {
+                    MessageBox.Show("Invalid character file (no name found).");
+                    return;
+                }
+
+                bool exists = profiles.Any(p => p["SelectedFeats"]?["Root:Identity"]?["Name"]?.ToString() == newName);
+                if (exists)
+                {
+                    var result = MessageBox.Show(
+                   $"A character named \"{newName}\" already exists. Import duplicate?",
+                   "Duplicate Character",
+                   MessageBoxButtons.YesNo,
+                   MessageBoxIcon.Warning
+               );
+
+                    if (result == DialogResult.No)
+                    {
+                        lblStatus.Text = $"Import canceled.";
+                        return;
+                    }
+                }
+
+                profiles.Add(newProfile);
+                characterLibrary["Profiles"] = profiles;
+
+                File.WriteAllText(libraryPath, characterLibrary.ToString(Formatting.Indented));
+                lstCharacters.Items.Add(newName);
+
+                lblStatus.Text = $"Imported {newName}.";
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int selectedIndex = lstCharacters.SelectedIndex;
+
+            if (selectedIndex < 0 || selectedIndex >= profiles.Count)
+            {
+                MessageBox.Show("Please select a valid character to delete.");
+                return;
+            }
+
+            // Get the selected character name for confirmation display
+            string selectedName = lstCharacters.SelectedItem.ToString();
+
+            var confirm = MessageBox.Show(
+                $"Are you sure you want to delete \"{selectedName}\"?",
+                "Confirm Delete",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning
+            );
+
+            if (confirm == DialogResult.Yes)
+            {
+                // Remove by index
+                profiles.RemoveAt(selectedIndex);
+                characterLibrary["Profiles"] = profiles;
+
+                // Save updated JSON
+                File.WriteAllText(libraryPath, characterLibrary.ToString(Formatting.Indented));
+
+                // Update UI
+                lstCharacters.Items.RemoveAt(selectedIndex);
+                lblStatus.Text = $"Deleted {selectedName}.";
+            }
+        }
+
+        private void btnOpenLibrary_Click(object sender, EventArgs e)
+        {
+            string defaultPath = Path.Combine(
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+        "Dawnsbury"
+    );
+
+            OpenFileDialog openDialog = new OpenFileDialog
+            {
+                Title = "Select characterLibrary.json",
+                Filter = "JSON Files (*.json)|*.json",
+                InitialDirectory = Directory.Exists(defaultPath) ? defaultPath : Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                FileName = "characterLibrary.json"
+            };
+
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                libraryPath = openDialog.FileName;
+
+                try
+                {
+                    string json = File.ReadAllText(libraryPath);
+                    characterLibrary = JObject.Parse(json);
+                    profiles = (JArray)(characterLibrary["Profiles"] ?? new JArray());
+
+                    lstCharacters.Items.Clear();
+
+                    foreach (var profile in profiles)
+                    {
+                        var name = profile["SelectedFeats"]?["Root:Identity"]?["Name"]?.ToString();
+                        lstCharacters.Items.Add(string.IsNullOrWhiteSpace(name) ? "[Unnamed Character]" : name);
+                    }
+
+                    if (profiles.Count > 0)
+                    {
+                        // Enable buttons now that a library is loaded
+                        btnExport.Enabled = true;
+                        btnDelete.Enabled = true;
+                        btnImport.Enabled = true;
+                        btnMoveDown.Enabled = true;
+                        btnMoveUp.Enabled = true;
+
+                        lblStatus.Text = $"Loaded {profiles.Count} character(s).";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Selected file is not a Character Library or contains 0 characters.","Error");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error reading character library: " + ex.Message);
+                    lblStatus.Text = "Failed to load character file.";
+                }
+            }
+        }
+
+        private void btnMoveUp_Click(object sender, EventArgs e)
+        {
+            int index = lstCharacters.SelectedIndex;
+            if (index > 0)
+            {
+                // Swap profiles in JArray
+                var tempProfile = profiles[index];
+                profiles[index] = profiles[index - 1];
+                profiles[index - 1] = tempProfile;
+
+                // Swap names in ListBox
+                var tempName = lstCharacters.Items[index];
+                lstCharacters.Items[index] = lstCharacters.Items[index - 1];
+                lstCharacters.Items[index - 1] = tempName;
+
+                // Select the moved item
+                lstCharacters.SelectedIndex = index - 1;
+
+                // Save changes
+                characterLibrary["Profiles"] = profiles;
+                File.WriteAllText(libraryPath, characterLibrary.ToString(Formatting.Indented));
+            }
+        }
+        private void lstCharacters_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = lstCharacters.SelectedIndex;
+            btnMoveUp.Enabled = index > 0;
+            btnMoveDown.Enabled = index >= 0 && index < lstCharacters.Items.Count - 1;
+        }
+
+        private void btnMoveDown_Click(object sender, EventArgs e)
+        {
+            int index = lstCharacters.SelectedIndex;
+            if (index >= 0 && index < lstCharacters.Items.Count - 1)
+            {
+                // Swap profiles in JArray
+                var tempProfile = profiles[index];
+                profiles[index] = profiles[index + 1];
+                profiles[index + 1] = tempProfile;
+
+                // Swap names in ListBox
+                var tempName = lstCharacters.Items[index];
+                lstCharacters.Items[index] = lstCharacters.Items[index + 1];
+                lstCharacters.Items[index + 1] = tempName;
+
+                // Select the moved item
+                lstCharacters.SelectedIndex = index + 1;
+
+                // Save changes
+                characterLibrary["Profiles"] = profiles;
+                File.WriteAllText(libraryPath, characterLibrary.ToString(Formatting.Indented));
+            }
         }
     }
 }
