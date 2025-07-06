5efe80987f913cd8e6ce5c948fb8f3b654255dae
Chuji <64228911+Choojermelon@users.noreply.github.com>
2025-07-06 20:31:02 +0100

    Test

--- DawnsburyCharacterManager/Program.cs
+++ DawnsburyCharacterManager/Program.cs
@@ -17,6 +17,7 @@ namespace DawnsburyCharacterManager
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // Test
         }
     }
 }
